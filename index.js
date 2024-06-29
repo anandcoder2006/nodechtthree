6d5e25adecfaeef3d600d001c6199fd0b8c1b1ce
Many <anandcoder2006@gmail.com>
2024-06-29 12:40:34 +0530

    Add File System

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const { writeFile } = require('fs/promises');
+async function writeToFile(fileName, data) {
+  try {
+    await writeFile(fileName, data);
+    console.log(`Wrote data to ${fileName}`);
+  } catch (error) {
+    console.error(`Got an error trying to write the file: ${error.message}`);
+  }
+}
+writeToFile('friends.txt', 'Bob');
